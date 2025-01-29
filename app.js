3a3d26394ad8e878763bbbe6cb576e7c12c4afa0
Anher <mistrydeepak1028@gmail.com>
2025-01-29 17:21:12 +0530

    first commit

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store=MongoStore.create({
   mongoUrl:dbUrl,
   crypto:{
-    secret:"mysupersecretcode",
+    secret:process.env.SECRET,
   
   },
   touchAfter: 24*3600,
@@ -62,7 +62,7 @@ store.on("error",()=>{
 })
 const sessionOptions = {   //used to store some info of user from current session
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie:{
